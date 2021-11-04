bf0e17d3fcce5339fe3b7761dfc482a796dca74a
Benjamin Kahl <kahl@mpib-berlin.mpg.de>
2021-11-04 16:06:11 +0100

    AVR_NET now substitutes the AVR_Behaviour base class completely

--- Packages/com.avr.core/Runtime/Scripts/Core/AVR_Behaviour.cs
+++ Packages/com.avr.core/Runtime/Scripts/Core/AVR_Behaviour.cs
@@ -10,7 +10,12 @@ namespace AVR.Core {
     /// Monobehaviour but with an added URL button to a documentation page.
     /// </summary>
     [AVR.Core.Attributes.DocumentationUrl("class_a_v_r_1_1_core_1_1_a_v_r___behaviour.html")]
-    public abstract class AVR_Behaviour : MonoBehaviour
+    public abstract class AVR_Behaviour :
+#if AVR_NET
+        Unity.Netcode.NetworkBehaviour
+#else
+        MonoBehaviour
+#endif
     {
         /// <summary>
         /// The current AVR_PlayerRig instance in the scene
