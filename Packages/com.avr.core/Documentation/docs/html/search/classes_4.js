c7958ec1d011eaeb91b28b0f00da19f8f1d1a3b9
Helliaca <benjaminkahl19@gmail.com>
2022-03-01 00:23:26 +0100

    Added default sounds to GrabbableObjectTypes and regenereated docs

--- Packages/com.avr.core/Documentation/docs/html/search/classes_4.js
+++ Packages/com.avr.core/Documentation/docs/html/search/classes_4.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['geom',['Geom',['../class_a_v_r_1_1_core_1_1_utils_1_1_geom.html',1,'AVR::Core::Utils']]],
+  ['grabbableobjectsounddata',['GrabbableObjectSoundData',['../struct_a_v_r_1_1_phys_1_1_grabbable_object_sound_data.html',1,'AVR::Phys']]],
   ['grabbableobjecttype',['GrabbableObjectType',['../class_a_v_r_1_1_phys_1_1_grabbable_object_type.html',1,'AVR::Phys']]],
   ['grablocation',['GrabLocation',['../class_a_v_r_1_1_phys_1_1_grab_location.html',1,'AVR::Phys']]]
 ];
